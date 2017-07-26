6fa37ec9421e4e2b3f3247a959493052f8dd7b28
Loic <loic.hamou@free.fr>
2017-07-26 12:07:22 +0200

    Init

--- exemple.php
+++ exemple.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Utilisateur
- * Date: 26/07/2017
- * Time: 09:49
- */
+ini_set("display_errors", "1" );
+
+
+
+require('fileCodeInject.php');
+
+$fci = new FileCodeInject('exemple.txt');
+
+echo '<pre>';
+print_r($fci->getContent());
+echo '</pre>';
+
+$fci->injectCode('NEW','MEGA','after',true,true);
+
+echo '<hr>';
+
+
+echo '<pre>';
+print_r($fci->getContent());
+echo '</pre>';
--- fileCodeInject.php
+++ fileCodeInject.php
@@ -1,12 +1,63 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Utilisateur
+ * User: LoicHa
  * Date: 26/07/2017
  * Time: 09:46
  */
 
-class fileCodeInject
+class FileCodeInject
 {
+	private $filename = false;
+	private $filenameContent = null;
+	function __construct($file)
+	{
+		if(file_exists($file))
+			$this->filename = $file;
+	}
 	
+	
+	public function getContent()
+	{
+		$this->filenameContent = file_get_contents($this->filename);
+		return $this->filenameContent;
+	}
+	
+	public function injectCode($code, $find, $position = 'before', $append = true, $newline = true)
+	{
+		$newline = $newline ? PHP_EOL  : '';
+		if(!$append)
+		{
+			file_put_contents($this->filename,$code);
+			$this->filenameContent = $code;
+			return;
+		}
+		if($position == 'top')
+		{
+			$this->filenameContent = $code.$newline.$this->filenameContent;
+			file_put_contents($this->filename,$this->filenameContent);
+			return;
+		}
+		if($position == 'bottom')
+		{
+			$this->filenameContent = $this->filenameContent.$newline.$code;
+			file_put_contents($this->filename,$this->filenameContent);
+			return;
+		}
+		if($find)
+		{
+			
+			if($position == 'before')
+			{
+				$this->filenameContent =  str_replace($find, $code . $newline . $find, $this->filenameContent);
+				file_put_contents($this->filename,$this->filenameContent);
+				return;
+			}
+			elseif($position == 'after')
+			{
+				$this->filenameContent =  str_replace($find, $find. $newline  .$code , $this->filenameContent);
+				file_put_contents($this->filename,$this->filenameContent);
+			}
+		}
+	}
 }
